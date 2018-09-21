dbbe16d3153fdac4ce6df9390d1e45ea6a9a1470
mathias schober <m.schober@intact.cc>
2018-09-21 11:41:51 +0200

    initial commit

---
+++ MLNETTutorial/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+
+namespace MLNETTutorial
+{
+    class Program
+    {
+        public class IrisData
+        {
+            [Column("0")]
+            public float SepalLength;
+
+            [Column("1")]
+            public float SepalWidth;
+
+            [Column("2")]
+            public float PetalLength;
+
+            [Column("3")]
+            public float PetalWidth;
+
+            [Column("4")]
+            [ColumnName("Label")]
+            public string Label;
+        }
+
+        public class IrisPrediction
+        {
+            [ColumnName("PredictedLabel")]
+            public string PredictedLabels;
+        }
+        static void Main(string[] args)
+        {
+            var pipeline = new LearningPipeline();
+
+            string dataPath = "iris-data.txt";
+            pipeline.Add(new TextLoader(dataPath).CreateFrom<IrisData>(separator: ','));
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
