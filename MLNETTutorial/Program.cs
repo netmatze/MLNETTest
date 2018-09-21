d9476140bcb5b16602f219a84d0fd2258eee3d55
mathias schober <m.schober@intact.cc>
2018-09-21 11:52:19 +0200

    tutorial completed

--- MLNETTutorial/Program.cs
+++ MLNETTutorial/Program.cs
@@ -37,12 +37,28 @@ namespace MLNETTutorial
         {
             var pipeline = new LearningPipeline();
 
-            string dataPath = "iris-data.txt";
+            string dataPath = "iris.data";
             pipeline.Add(new TextLoader(dataPath).CreateFrom<IrisData>(separator: ','));
 
+            pipeline.Add(new Dictionarizer("Label"));
 
+            pipeline.Add(new ColumnConcatenator("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"));
 
-            Console.WriteLine("Hello World!");
+            pipeline.Add(new StochasticDualCoordinateAscentClassifier());
+
+            pipeline.Add(new PredictedLabelColumnOriginalValueConverter() { PredictedLabelColumn = "PredictedLabel" });
+
+            var model = pipeline.Train<IrisData, IrisPrediction>();
+
+            var prediction = model.Predict(new IrisData()
+            {
+                SepalLength = 3.3f,
+                SepalWidth = 1.6f,
+                PetalLength = 0.2f,
+                PetalWidth = 5.1f,
+            });
+
+            Console.WriteLine($"Predicted flower type is: {prediction.PredictedLabels}");
             Console.ReadLine();
         }
     }
